2e2b571c8a67ebe518f1264c61b3403bfc583c11
junzhi9820 <52389715+junzhi9820@users.noreply.github.com>
2022-08-21 17:07:59 +0800

    environment update

---
+++ composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'cakephp/cakephp' => array(
+            'pretty_version' => '2.10.24',
+            'version' => '2.10.24.0',
+            'reference' => 'cf14e6546ec44e3369e3531add11fdb946656280',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cakephp/cakephp',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
